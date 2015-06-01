2c281659d35c9a665100f5c38071f68e541bf088
Brian Hamburg <brian.hamburg@jefferson.edu>
2015-06-01 11:17:17 -0400

    completed Simple Web Server project

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var http = require('http');
 var url = require('url');
 var path = require('path');
-var fs = require('fs')
+var fs = require('fs');
 
 // Object of Mime Types
 var mimeTypes = {
@@ -15,9 +15,35 @@ var mimeTypes = {
 };
 
 // Create Server
-http.createServer(function(){
+http.createServer(function(req,res){
     var uri = url.parse(req.url).pathname;
-    var filename = path.join(process.cwd(),unescape(uri))
-    console.log();
-});
+    var fileName = path.join(process.cwd(),unescape(uri));
+    console.log('Loading ' + uri);
+    var stats;
 
+    try {
+        stats = fs.lstatSync(fileName);
+    } catch(e) {
+        res.writeHead(404, {'Content-type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+
+    if(stats.isFile()) {
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+        res.writeHead(200, {'Content-type': mimeType});
+
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    } else if(stats.isDirectory()) {
+        res.writeHead(302, {
+            'Location' : 'index.html'
+        });
+        res.end();
+    } else {
+        res.writeHead(500, {'Content-type': 'text/plain'});
+        res.write('500 Internal Error');
+        res.end();
+    }
+}).listen(3000);
